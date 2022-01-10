09f4206e9ec42602d0f3cda2247e7f5e917e360b
iddiaz <correo@ivandiazdiaz.com>
2022-01-10 12:44:34 +0100

    Cnfiguración redux

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,30 @@
+import { types } from './../types/types';
+
+/*
+
+   {
+      uid:adshkf70897a8df,
+      name:'Ivan'
+   }
+
+*/
+
+// reducer de la autenticacion
+export const authReducer = ( state = {}, action) => {
+   
+   switch ( action.type ) {
+      case types.login:
+         
+         return {
+            uid: action.payload.uid,
+            name: action.payload.displayName
+         }
+   
+      case types.logout:         
+         return {}
+   
+      default:      
+         return state;
+   }
+
+}
