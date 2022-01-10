f2455ae08530720ef1b3b4b903c61f727948016f
iddiaz <correo@ivandiazdiaz.com>
2022-01-10 14:20:53 +0100

    configuración de la accion de login y dispatch

---
+++ src/actions/auth.js
@@ -0,0 +1,23 @@
+
+import { types } from './../types/types';
+
+
+// export const login = (uid, displayName) =>{
+//    return {
+//       type: types.login,
+//       payload: {
+//          uid,
+//          displayName
+//       }
+//    }
+// } 
+// ===
+export const login = (uid, displayName) => ({ 
+  
+      type: types.login,
+      payload: {
+         uid,
+         displayName
+      }
+   
+})
--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,13 +1,50 @@
 import { Link } from "react-router-dom";
+import { useForm } from './../../hooks/useForm';
+import {useDispatch}  from 'react-redux';
+import { login } from './../../actions/auth';
 
 export const LoginScreen = () => {
+   //useDispach => Da acceso al dispach y poder hacer dispach de acciones.
+   const dispatch = useDispatch();  
+
+
+   const [formValues, handleInputChange] = useForm({
+      email:'ivan@correo.com',
+      password:'123456'
+   })
+
+   const { email, password } = formValues;
+
+   const handleLogin = ( e ) =>{
+      e.preventDefault();
+      console.log(email,password);
+      dispatch( login(12345, 'pepe') );
+
+   }
+
+
+
    return (
       <>
          <h3 className="auth__title">Login</h3>
 
-         <form action="">
-            <input className="auth__input" type="text" placeholder="Email" name="email" autoComplete="off" />
-            <input className="auth__input" type="password" placeholder="Password" name="password" />
+         <form action="" onSubmit={handleLogin}>
+            <input 
+               className="auth__input" 
+               type="text" 
+               placeholder="Email" 
+               name="email" 
+               autoComplete="off"
+               value={email}
+               onChange={handleInputChange} />
+
+            <input 
+               className="auth__input" 
+               type="password" 
+               placeholder="Password" 
+               name="password"
+               value={password}
+               onChange={handleInputChange} />
 
             <button className="btn btn-primary btn-block" type="submit" >Login</button>
            
