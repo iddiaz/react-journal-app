a847b475afd72dd4edf850f1db04036ce0a5bd81
iddiaz <correo@ivandiazdiaz.com>
2022-01-20 12:16:46 +0100

    Configuración de entornos y variables de entorno

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -7,41 +7,42 @@ import { getFirestore } from 'firebase/firestore';
 import { GoogleAuthProvider } from 'firebase/auth';
  
 
+console.log( process.env );
 
-
-// Your web app's Firebase configuration
+// Your web app's Firebase configuration 
 //dev/prod
 const firebaseConfig = {
-  apiKey: "AIzaSyB0crEaaJBddscZHDMH86ku4peagAmX__k",
-  authDomain: "react-app-journal-36eb9.firebaseapp.com",
-  projectId: "react-app-journal-36eb9",
-  storageBucket: "react-app-journal-36eb9.appspot.com",
-  messagingSenderId: "109352522832",
-  appId: "1:109352522832:web:a4fd43f2f53a5d04eead55"
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID
 };
 
 //test
-const firebaseConfigTesting = {
-  apiKey: "AIzaSyCWrZf6jw-SlEKbZd-CSBzFQVrds2dwEcQ",
-  authDomain: "test-db-react-5e509.firebaseapp.com",
-  projectId: "test-db-react-5e509",
-  storageBucket: "test-db-react-5e509.appspot.com",
-  messagingSenderId: "735526459305",
-  appId: "1:735526459305:web:661b917d34f3ee56ba16b8"
-};
+// const firebaseConfigTesting = {
+//   apiKey: process.env.REACT_APP_APIKEY,
+//   authDomain: process.env.REACT_APP_AUTHDOMAIN,
+//   projectId: process.env.REACT_APP_PROJECTID,
+//   storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+//   appId: process.env.REACT_APP_APPID
+// };
 
-if( process.env.NODE_ENV === 'test' ){
+// if( process.env.NODE_ENV === 'test' ){
 
-    const app = initializeApp(firebaseConfigTesting);
+//     const app = initializeApp(firebaseConfigTesting);
     
-  } else {
-    //dev/prod
+//   } else {
+//     //dev/prod
 
-    const app = initializeApp(firebaseConfig);
-
-}
-
-console.log(process.env);
+//     const app = initializeApp(firebaseConfig);
+    
+//   }
+  
+  console.log(process.env);
+  const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase
 const db = getFirestore();
